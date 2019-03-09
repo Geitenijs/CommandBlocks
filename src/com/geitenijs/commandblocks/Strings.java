dc0a978d1617eb6f72798a561c6c1f1bf2c0dd1a
Geitenijs <git@geitenijs.com>
2019-03-09 20:31:25 +0100

    Bump version

--- src/com/geitenijs/commandblocks/Strings.java
+++ src/com/geitenijs/commandblocks/Strings.java
@@ -5,7 +5,7 @@ public class Strings {
     static final String PLUGIN = "CommandBlocks";
     static final String INTERNALPREFIX = "[CommandBlocks] ";
     public static final String GAMEPREFIX = "&cCommand&8Blocks &7// ";
-    public static final String VERSION = "1.1.9";
+    public static final String VERSION = "1.1.10";
     public static final String AUTHOR = "Geitenijs";
     public static final String COPYRIGHT = "2018-2019";
     static final int RESOURCEID = 62720;
