3bebde6347d65dce21d8bbbefe6850cc6561e931
Geitenijs <development@geitenijs.com>
2020-04-21 14:23:37 +0200

    Removed server software from main command

--- src/com/geitenijs/commandblocks/Strings.java
+++ src/com/geitenijs/commandblocks/Strings.java
@@ -6,7 +6,7 @@ public class Strings {
     static final String INTERNALPREFIX = "[CommandBlocks] ";
     public static final String DEBUGPREFIX = "[DEBUG] ";
     public static final String GAMEPREFIX = "&cCommand&8Blocks &7// ";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     public static final String AUTHOR = "Geitenijs";
     public static final String COPYRIGHT = "2018-2020";
     static final int RESOURCEID = 62720;
