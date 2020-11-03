0deb2f8d8d837a067858f24f138694c16b4eb614
Geitenijs <development@geitenijs.com>
2020-11-03 08:21:43 +0100

    1.16.4 support

--- src/com/geitenijs/commandblocks/Strings.java
+++ src/com/geitenijs/commandblocks/Strings.java
@@ -6,7 +6,7 @@ public class Strings {
     static final String INTERNALPREFIX = "[CommandBlocks] ";
     public static final String DEBUGPREFIX = "[DEBUG] ";
     public static final String GAMEPREFIX = "&cCommand&8Blocks &7// ";
-    public static final String VERSION = "1.2.5";
+    public static final String VERSION = "1.2.6";
     public static final String AUTHOR = "Geitenijs";
     public static final String COPYRIGHT = "2018-2020";
     static final int RESOURCEID = 62720;
