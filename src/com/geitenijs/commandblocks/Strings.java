119545c28c57310ce31d8fd408996d1d41fda4c5
Geitenijs <development@geitenijs.com>
2021-06-13 12:16:44 +0200

    1.17 support

--- src/com/geitenijs/commandblocks/Strings.java
+++ src/com/geitenijs/commandblocks/Strings.java
@@ -6,9 +6,9 @@ public class Strings {
     static final String INTERNALPREFIX = "[CommandBlocks] ";
     public static final String DEBUGPREFIX = "[DEBUG] ";
     public static final String GAMEPREFIX = "&cCommand&8Blocks &7// ";
-    public static final String VERSION = "1.2.6";
+    public static final String VERSION = "1.2.7";
     public static final String AUTHOR = "Geitenijs";
-    public static final String COPYRIGHT = "2018-2020";
+    public static final String COPYRIGHT = "2018-2021";
     static final int RESOURCEID = 62720;
     static final String WEBSITE = "https://www.spigotmc.org/resources/" + Strings.RESOURCEID;
     static final String ASCIILOGO = "" +
